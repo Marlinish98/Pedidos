203b83843fc6a588959b0195dc94cc9f9d18b7fb
Marlinish98 <alex2015drum@gmail.com>
2025-10-24 18:57:10 -0600

    creacion de herencias

---
+++ Herencia/Libro.cs
@@ -0,0 +1,18 @@
+public class Libro: Producto
+{
+    public string? ISBN { get; set; }
+
+    public string? Autor { get; set; }
+    
+    public int NumeroPaginas { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Numero de Paginas: {NumeroPaginas}");
+        Console.WriteLine(""); 
+    }
+    
+}
